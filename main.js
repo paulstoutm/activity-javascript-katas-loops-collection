62ed54ac084c1af1d37962701eabc045fb1464ba
unknown <paul@paulstoutcountry.com>
2020-05-12 14:37:09 -0400

    changed JavaS

--- main.js
+++ main.js
@@ -18,8 +18,14 @@
 ***
 ***/
 
-  console.log('%cKATA 02 RESULTS:', 'background: darkblue; color: white; font-weight: bold;') // These console.logs are just to help make the console log better organized and more readable
+let counter2 = 0
+  while (counter2 <= 20) {
+    console.log(counter2)
+    counter2 += 2
+  
 
+  console.log('%cKATA 02 RESULTS:', 'background: darkblue; color: white; font-weight: bold;') // These console.logs are just to help make the console log better organized and more readable
+}
 
   /* Write your KATA 02 code in place of THIS comment. */
 
@@ -29,7 +35,11 @@
 *** Log the odd numbers from 1 to 20. (1, 3, 5,...,17,19)
 ***
 ***/
-
+let counter3 = 1
+  while (counter3 <= 20) {
+    console.log(counter3)
+    counter3 += 2
+  }
   console.log('%cKATA 03 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
  
 
@@ -41,8 +51,14 @@
 *** Log the multiples of 5 up to 100. (5, 10, 15, ..., 95, 100)
 ***
 ***/
+console.log('%cKATA 04 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+let counter4 = 5
+  while (counter4 <= 100) {
+    console.log(counter4)
+    counter4 += 5
+  }
 
-  console.log('%cKATA 04 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+  
 
 
   /* Write your KATA 04 code in place of THIS comment. */
@@ -53,8 +69,14 @@
 *** Log all numbers up to 100 that are perfect squares. (1, 4, 9, ..., 81, 100)
 ***
 ***/
+console.log('%cKATA 05 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+let counter5 = 1
+  while (counter5 <= 10) {
+   console.log(counter5 **2)
+      counter5 += 1
+  }
 
-  console.log('%cKATA 05 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+  
 
 
   /* Write your KATA 05 code in place of THIS comment. */
@@ -65,8 +87,13 @@
 *** Log the numbers counting backwards from 20 to 1. (20, 19, 18, ..., 2, 1)
 ***
 ***/
-
-  console.log('%cKATA 06 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+console.log('%cKATA 06 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+let counter6 = 20
+  while (counter6 >= 1) {
+   console.log(counter6)
+      counter6 -= 1
+  }
+  
 
 
   /* Write your KATA 06 code in place of THIS comment. */
@@ -77,9 +104,14 @@
 *** Log the even numbers counting backwards from 20. (20, 18, 16, ..., 4, 2)
 ***
 ***/
+console.log('%cKATA 07 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
+let counter7 = 20
+  while (counter7 >= 1) {
+   console.log(counter7)
+      counter7 -= 2
+  }
 
-  console.log('%cKATA 07 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
-
+  
 
   /* Write your KATA 07 code in place of THIS comment. */
 
@@ -91,7 +123,11 @@
 ***/
 
   console.log('%cKATA 08 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
-
+  let counter8 = 19
+  while (counter8 >= 1) {
+   console.log(counter8)
+      counter8 -= 2
+  }
 
   /* Write your KATA 08 code in place of THIS comment. */
 
@@ -103,7 +139,10 @@
 ***/
 
   console.log('%cKATA 09 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
-
+  let counter9 = 100
+  while (counter9 >= 1) {
+   console.log(counter9)
+      counter9 -= 5
 
   /* Write your KATA 09 code in place of THIS comment. */
 
@@ -113,12 +152,16 @@
 *** Log the numbers that are perfect squares, counting down from 100. (100, 81, 64, ..., 4, 1)
 ***
 ***/
-
+  }
   console.log('%cKATA 10 RESULTS:', 'background: darkblue; color: white; font-weight: bold;')
-
-
+  let counter10 = 10
+  while (counter10 >= 1) {
+   console.log(counter10 **2)
+      counter10 -= 1
+  }
   /* Write your KATA 10 code in place of THIS comment. */
 
 
 
 
+  
